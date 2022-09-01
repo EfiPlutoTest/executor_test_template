78b16e3e109883727ec26b29f017b89a57191494
Aapo Romu <aapo.romu@eficode.com>
2022-09-01 15:18:23 +0300

    Add a service

--- executor_script.py
+++ executor_script.py
@@ -5,6 +5,8 @@ import json
 import logging as log
 from utils.common import LogManager
 
+from services import test_service
+
 if __name__ == '__main__':
     print("RUNNING....")
     log_manager = LogManager()
@@ -12,9 +14,12 @@ if __name__ == '__main__':
     log.info("Hello World!")
     log.info("Environment: %s", os.environ)
 
+    greeting = test_service.get_test_value(os.environ['TEST_FIELD'])
+
     result = {'result': {
         'projectKey': 'TSTPRJ',
-        'url': 'http://jira.yourcompany.example.com'
+        'url': 'http://jira.yourcompany.example.com',
+        'greeting': greeting
     }}
 
     with open(os.environ['RESULT_FILE'], 'w', encoding="utf8") as target_file:
---
+++ services/test_service.py
@@ -0,0 +1,3 @@
+
+def get_test_value(intput):
+    return f"Hello {intput}"
